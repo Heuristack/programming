ed84c310280292ca849aa796aab2e9449abee349
Lance LI <heuristack@gmail.com>
2017-12-07 19:18:48 +0800

    programming: use yaml to define messages and aiso for concurrency [orion-client]

---
+++ orion-client/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <yaml-cpp/yaml.h>
+
+int main()
+{
+  boost::asio::io_service io;
+
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+  t.wait();
+
+  YAML::Node node  = YAML::LoadFile("../etc/itch5.yaml");
+  std::cout << node << std::endl;
+
+  return 0;
+}
