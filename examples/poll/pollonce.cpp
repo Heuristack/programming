801dfb9161b83b98caca720c81d3421f2b77fba3
Lance LI <heuristack@gmail.com>
2018-11-20 19:49:20 +0800

    programming: add example of using poll to monitor standard input and output for read and write seperately [poll]

---
+++ examples/poll/pollonce.cpp
@@ -0,0 +1,34 @@
+#include <sys/poll.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main (void)
+{
+    struct pollfd fds[2];
+
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+    fds[1].fd = STDOUT_FILENO;
+    fds[1].events = POLLOUT;
+
+    if (int n = poll(fds, 2, 5); n > 0) {
+        if (fds[0].revents & POLLIN )  {
+            cout << "0 is ready" << endl;
+        }
+        if (fds[1].revents & POLLOUT) {
+            cout << "1 is ready" << endl;
+        }
+    }
+    else if (n == 0) {
+        cout << "timeout ..." << endl;
+    }
+    else {
+        cout << strerror(errno) << endl;
+    }
+
+    return 0;
+}
+
