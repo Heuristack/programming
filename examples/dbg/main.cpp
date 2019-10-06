ddb48b4a033ba7de6473372cdc86f48d813377fe
Lance LI <heuristack@gmail.com>
2019-10-06 15:00:32 +0800

    programming: add example of dbg to print object of various types [debug]

---
+++ examples/dbg/main.cpp
@@ -0,0 +1,34 @@
+#include "dbg.h"
+using namespace std;
+
+template <typename string_t>
+class string_wrapper
+{
+public:
+    typedef string_t string_type;
+    typedef string_wrapper<string_type> this_type;
+
+public:
+    string_wrapper(string_type const & s) : m(s) {}
+    string_wrapper() = default;
+
+private:
+    string_type m = "Null";
+
+public:
+    friend ostream & operator<<(ostream & s, this_type const & o)
+    {
+        return s << o.m;
+    }
+};
+
+int main()
+{
+    long long integer = 0xFF;
+    string message {"Hello,World!"};
+    string_wrapper<string> wrapper(message);
+    dbg(integer);
+    dbg(message);
+    dbg(wrapper);
+}
+
