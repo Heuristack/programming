cfef0b98c5c63f6a58e46c51b413210e0cbfada6
Lance LI <heuristack@gmail.com>
2020-03-10 22:54:13 +0800

    programming: improve the implementation of linked list using node and link structure [algo]

--- algo/linked_list.cpp
+++ algo/linked_list.cpp
@@ -1,58 +1,98 @@
+#include <initializer_list>
 #include <iostream>
 
-template <typename value_t>
+using namespace std;
+
+template <typename data_t>
 struct node
 {
-    using value_type = value_t;
-    using node_type = node<value_type>;
+    using date_type = data_t;
+    using node_type = node<date_type>;
     using link_type = node_type *;
 
-    node(value_type v, link_type n) : value(v), next(n) {}
-    explicit node(value_type v) : node(v,nullptr) {}
+    node(date_type data, link_type n) : data(data), next(n) {}
+    explicit node(date_type data) : node(data,nullptr) {}
 
-    value_type value;
-    link_type next{nullptr};
+    date_type data;
+    link_type next;
 };
 
-template <typename value_t>
-using link = typename node<value_t>::link_type;
+template <typename node_t>
+using list = typename node_t::link_type;
 
-template <typename value_t>
-using list = link<value_t>;
-
-template <typename value_t>
-bool is_empty(list<value_t> const & head)
+template <typename data_t>
+auto create_list(initializer_list<data_t> const & init)
 {
-    return head == nullptr;
+    list<node<data_t>> head = nullptr;
+    list<node<data_t>> tail = head;
+    assert(!tail);
+    for (auto const & data : init) {
+        if (!tail) {
+            tail = new node<data_t>(data);
+            head = tail;
+        }
+        else {
+            tail->next = new node<data_t>(data);
+            tail = tail->next;
+        }
+    }
+    return head;
 }
 
-template <typename value_t>
-auto traverse(list<value_t> const & head)
+template <typename list_t, typename func_t>
+auto traverse(list_t const & head, func_t func)
 {
-    for (auto p = head; p != nullptr; p = p->next) {
-        std::cout << p->value << std::endl;
+    for (auto i = head; i != nullptr; i = i->next) {
+        func(i->data);
     }
 }
 
-template <typename value_t>
-auto traverse_recursive(list<value_t> const & head)
+template <typename list_t, typename pred_t>
+auto remove_if(list_t & head, pred_t pred)
 {
-    if (head == nullptr) return;
-    else {
-        traverse_recursive<value_t>(head->next);
-        std::cout << head->value << std::endl;
+    if (!head) return;
+    assert(head);
+    while (head && pred(head->data)) {
+        auto x = head;
+        head = head->next;
+        delete x;
     }
+    if (!head) return;
+    assert(head && !pred(head->data));
+    auto prev_node = head;
+    auto this_node = head->next;
+    while (this_node) {
+        if (pred(this_node->data)) {
+            prev_node->next = this_node->next;
+            delete this_node;
+            this_node = prev_node->next;
+        }
+        else {
+            prev_node = this_node;
+            this_node = this_node->next;
+        }
+    }
+}
+
+template <typename list_t>
+auto delete_list(list_t & head)
+{
+    remove_if(head,[](auto const &){ return true; });
 }
 
 int main()
 {
-    using node_type = node<int>;
-    using list_type = list<node_type::value_type>;
+    auto list = create_list<int>({3,1,4,1,5,9});
+    traverse(list,[](auto const & data){ cout << data; });
+    cout << endl;
 
-    list_type head = new node_type(3, new node_type(1, new node_type(4,
-            new node_type(1, new node_type(5,new node(9))))));
 
-    traverse<node_type::value_type>(head);
-    traverse_recursive<node_type::value_type>(head);
+    remove_if(list,[](auto const & data){ return data == 1; });
+    traverse(list,[](auto const & data){ cout << data; });
+    cout << endl;
+
+    delete_list(list);
+    traverse(list,[](auto const & data){ cout << data; });
+    cout << endl;
 }
 
