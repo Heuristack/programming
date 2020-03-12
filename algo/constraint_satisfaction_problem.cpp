9e6382d253c0581537f6fd63249f0aaec18b9aea
Lance LI <heuristack@gmail.com>
2020-03-12 23:07:43 +0800

    programming: add problem space model and csp [algo]

---
+++ algo/constraint_satisfaction_problem.cpp
@@ -0,0 +1,10 @@
+template<typename T, template<typename>typename Container>
+struct ConstraintSatisfactionProblem
+{
+    Container<T> variables;
+    Container<T> values;
+};
+
+int main()
+{}
+
---
+++ algo/problem_space_model.cpp
@@ -0,0 +1,28 @@
+template <typename State, typename Operator, template<typename>typename Set>
+struct ProblemSpace
+{
+    void dfs(State v)
+    {
+        if (v == goal) {
+            return;
+        }
+        for (auto w : expand(v)) {
+            dfs(w);
+        }
+    }
+
+    Set<State> expand(State s)
+    {
+        Set<State> children;
+        return children;
+    }
+
+    State initial;
+    State goal;
+    Set<State> states;
+    Set<Operator> operators;
+};
+
+int main()
+{}
+
