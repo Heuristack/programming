e72f3fc9b9654c6e4d69a4bce8e1e03d12244fe2
Lance LI <heuristack@gmail.com>
2020-03-22 23:13:00 +0800

    programming: generate n-permutation using recursion [algo]

--- algo/permutation.cpp
+++ algo/permutation.cpp
@@ -1,16 +1,35 @@
-#include <algorithm>
-#include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-// todo : implement permutation algorithm in backtracking
+auto permutation(vector<int> s, int n) -> vector<vector<int>>
+{
+    static vector<vector<int>> perms;
+    static vector<int> path;
+
+    if (!n) {
+        perms.push_back(path);
+        return perms;
+    }
+
+    for (auto i = begin(s); i != end(s); i++) {
+        vector<int> c(begin(s),i);
+        copy(next(i),end(s),back_inserter(c));
+        path.push_back(*i);
+        permutation(c,n-1);
+        path.pop_back();
+    }
+
+    return perms;
+}
 
 int main()
 {
-    string s = "abc";
-    sort(begin(s),end(s));
-    do { cout << s << endl; }
-    while (next_permutation(begin(s),end(s)));
+    for (auto const & s : permutation({1,2,3},2)) {
+        for (auto const & e : s) cout << e;
+        cout << endl;
+    }
 }
 
