1aa74effee48c2437c1d62022547c92db22cc8c5
Lance LI <heuristack@gmail.com>
2020-04-14 15:26:46 +0800

    programming: perfect forwarding is still misterious to me [C++]

---
+++ lang/template/perfect_forward.cpp
@@ -0,0 +1,42 @@
+#include <type_traits>
+#include <iostream>
+#include <string>
+#include <utility>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::remove_reference;
+using std::move;
+
+namespace explicit_return {
+template <typename type> type&& forward(typename remove_reference<type>::type&  value) { cout << "ecp:"; return static_cast<type&&>(value); }
+template <typename type> type&& forward(typename remove_reference<type>::type&& value) { cout << "emv:"; return static_cast<type&&>(value); }
+}
+
+namespace implicit_return {
+template <typename type> decltype(auto) forward(typename remove_reference<type>::type&  value) { cout << "icp:"; return static_cast<type&&>(value); } // note : decltype(auto) is necessary
+template <typename type> decltype(auto) forward(typename remove_reference<type>::type&& value) { cout << "imv:"; return static_cast<type&&>(value); } //        to preserve reference-ness!
+}
+
+void print(string const & value) { cout << "c:" << value << endl; }
+void print(string &  value)      { cout << "l:" << value << endl; }
+void print(string && value)      { cout << "r:" << value << endl; }
+
+template <typename type> void explicit_print(type && value) { print(explicit_return::forward<type>(value)); }
+template <typename type> void implicit_print(type && value) { print(implicit_return::forward<type>(value)); }
+
+int main()
+{
+    string a("Perfect!");
+    string b("Forward!");
+
+    explicit_print(a);
+    explicit_print(move(a));
+    explicit_print(move(string("Really?")));
+
+    implicit_print(b);
+    implicit_print(move(b));
+    implicit_print(move(string("Really?")));
+}
+
