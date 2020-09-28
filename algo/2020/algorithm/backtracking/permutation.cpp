97b22569f85416a366b4a4c625693a71521770d0
Lance LI <heuristack@gmail.com>
2020-09-28 21:35:47 +0800

    programming: improve the n-permutations generation implementation [algo]

--- algo/2020/algorithm/backtracking/permutation.cpp
+++ algo/2020/algorithm/backtracking/permutation.cpp
@@ -16,7 +16,7 @@ auto remove_copy_it(iterator p, iterator r, inserter b, iterator k) -> void
 }
 
 template <typename iterator, typename return_type = vector<typename iterator_traits<iterator>::value_type>>
-auto remove_clone_it(iterator p, iterator r, iterator k) -> return_type
+auto remove_copy_it(iterator p, iterator r, iterator k) -> return_type
 {
     return_type v;
     remove_copy_it(p,r,back_inserter(v),k);
@@ -35,7 +35,7 @@ auto permute(vector<int> s, int n) -> vector<vector<int>> // note : generate all
 
     for (auto i = begin(s); i != end(s); i++) {
         path.push_back(*i);
-        permute(remove_clone_it(begin(s),end(s),i),n-1);
+        permute(remove_copy_it(begin(s),end(s),i),n-1);
         path.pop_back();
     }
 
