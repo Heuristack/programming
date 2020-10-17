e48d006ceb6a7c2774db4ca8e156d44931bc693c
Lance LI <heuristack@gmail.com>
2020-10-17 15:02:59 +0800

    programming: state space model in exploration of 2-d space [algo]

---
+++ algo/2020/algorithm/brute_force/state_space_model.hpp
@@ -0,0 +1,46 @@
+template <typename state_t, typename operate_t>
+class state_space_model
+{
+public:
+    typedef state_t state_type;
+    typedef operate_t operate_type;
+
+public:
+    state_space_model(state_type i, state_type g) : init(i), goal(g) {}
+    state_space_model() = default;
+
+public:
+    virtual auto visit(state_type) -> void {}
+    virtual auto next(state_type s, operate_type o) -> optional<state_type> = 0;
+
+    auto expand(state_type s) -> vector<state_type>
+    {
+        vector<state_type> next_states;
+        for (auto const & o : operates) {
+            auto n = next(s,o);
+            if (n.has_value()) {
+                next_states.push_back(n.value());
+            }
+        }
+        return next_states;
+    }
+
+public:
+    virtual auto search(state_type s) -> void
+    {
+        visit(s);
+        if (s == goal) return;
+        for (auto const & s : expand(s)) {
+            search(s);
+        }
+    }
+
+public:
+    set<state_type> states;
+    set<operate_type> operates;
+
+public:
+    state_type init{};
+    state_type goal{};
+};
+
