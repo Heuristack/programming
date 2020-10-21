b2145f4edb2cc9f9a8ce9f5bd0d8a14a29ff2012
Lance LI <heuristack@gmail.com>
2020-10-21 23:32:01 +0800

    programming: n-queens n-perm method - 2 (8-queens = 92) [algo]

--- algo/2020/algorithm/backtracking/n-queens.cpp
+++ algo/2020/algorithm/backtracking/n-queens.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <limits>
+#include <iostream>
 
 using namespace std;
 
@@ -75,6 +76,19 @@ ostream & operator << (ostream & s, node const & e)
     return s;
 }
 
+auto generate(int i, int j, int n) -> vector<pair<int,int>>
+{
+    vector<pair<int,int>> v;
+    for (int d = 1; d <= n-1; d++) {
+        for (auto [p,q] : vector<pair<int,int>>{{i-d,j+d},{i+d,j-d},{i-d,j-d},{i+d,j+d}}) {
+            if ((0 <= p) && (p < n) && (0 <= q) && (q < n)) {
+                v.emplace_back(p,q);
+            }
+        }
+    }
+    return v;
+}
+
 auto generate(node const & e)
 {
     vector<node> v;
@@ -84,6 +98,14 @@ auto generate(node const & e)
         q.j = e.c[i];
         n.c.erase(begin(n.c)+i);
         n.b[q.i][q.j] = 1;
+        bool attack = 0;
+        for (auto [a,b] : generate(q.i,q.j,n.n)) {
+            if (n.b[a][b] == 1) {
+                attack = 1;
+                break;
+            }
+        }
+        if (attack) continue;
         v.push_back(n);
 
     }
@@ -95,6 +117,7 @@ auto explore(node const & e) -> int
     static int n = 0;
     if (e.c.size() == 0) {
         n++;
+//      cout << e;
     }
     for (auto const & n: generate(e)) {
         explore(n);
