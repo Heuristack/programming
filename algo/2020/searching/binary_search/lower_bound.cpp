7a0d86e7df301c9c02697275efb36cde04c670e2
Lance LI <heuristack@gmail.com>
2020-11-01 11:16:30 +0800

    programming: adding minimum spanning tree algorithms [algo]

--- algo/2020/searching/binary_search/lower_bound.cpp
+++ algo/2020/searching/binary_search/lower_bound.cpp
@@ -25,7 +25,7 @@ int main()
 {
     vector<int> v {-14,-10,2,108,108,243,285,285,285,401};
     assert(lower_bound_search(begin(v),end(v),285) == lower_bound(begin(v),end(v),285));
-    // todo : fix it
+    // todo : fix me
     cout << *(lower_bound_search(begin(v),end(v),250)) << endl;
     cout << *(lower_bound(begin(v),end(v),250)) << endl;
 }
