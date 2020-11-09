5308d46241ea6c08860d30bb22ac15386161d732
Lance LI <heuristack@gmail.com>
2020-11-09 21:51:08 +0800

    programming: hash and merge vector of elements [quant]

--- algo/2020/structure/hash_table/hash_homogeneous_aggregate.cpp
+++ algo/2020/structure/hash_table/hash_homogeneous_aggregate.cpp
@@ -5,20 +5,20 @@
 
 using namespace std;
 
-struct ContactList
+struct contact_graph
 {
-    struct Equal
+    struct equal
     {
-        auto operator() (ContactList const a, ContactList const & b) const
+        auto operator() (contact_graph const a, contact_graph const & b) const
         {
             return unordered_set<string>(begin(a.names),end(a.names))
                 == unordered_set<string>(begin(b.names),end(b.names));
         }
     };
 
-    struct Hash
+    struct hash
     {
-        auto operator() (ContactList const & c) const
+        auto operator() (contact_graph const & c) const
         {
             size_t code;
             for (auto const & n : unordered_set<string>(begin(c.names),end(c.names))) {
@@ -28,23 +28,23 @@ struct ContactList
         }
     };
 
-    ContactList(vector<string> const & n) : names(n) {}
+    contact_graph(vector<string> const & n) : names(n) {}
     vector<string> names;
 };
 
-auto MergeContactLists(vector<ContactList> const & contacts)
+auto merge(vector<contact_graph> const & contacts)
 {
-    unordered_set<ContactList, ContactList::Hash, ContactList::Equal> unique_contacts(begin(contacts), end(contacts));
-    return vector<ContactList>(begin(unique_contacts),end(unique_contacts));
+    unordered_set<contact_graph, contact_graph::hash, contact_graph::equal> unique_contacts(begin(contacts), end(contacts));
+    return vector<contact_graph>(begin(unique_contacts),end(unique_contacts));
 }
 
 int main()
 {
-    ContactList A(vector<string>{"Aureliano","Arcadio","Amaranta"});
-    ContactList B(vector<string>{"Aureliano","Arcadio"});
-    ContactList C(vector<string>{"Aureliano","Arcadio"});
-    for (auto const & L : MergeContactLists(vector<ContactList>{A,B})) {
-        for (auto const & n : L.names) {
+    contact_graph generation1(vector<string>{"Aureliano","Arcadio","Amaranta"});
+    contact_graph generation2(vector<string>{"Aureliano","Arcadio"});
+    contact_graph generation3(vector<string>{"Aureliano","Arcadio"});
+    for (auto const & g : merge(vector<contact_graph>{generation1,generation2,generation3})) {
+        for (auto const & n : g.names) {
             cout << n << " ";
         }
         cout << endl;
