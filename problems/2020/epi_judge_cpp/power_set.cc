5d8737a0996dac25d28117766a2cc56164e1c316
Lance LI <heuristack@gmail.com>
2020-11-22 23:58:38 +0800

    programming: power set generation [prob]

--- problems/2020/epi_judge_cpp/power_set.cc
+++ problems/2020/epi_judge_cpp/power_set.cc
@@ -1,11 +1,40 @@
 #include <vector>
+#include <bitset>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::bitset;
 
-vector<vector<int>> GeneratePowerSet(const vector<int>& input_set) {
-  // TODO - you fill in here.
-  return {};
+inline auto generate_powerset_0(vector<int> s) -> vector<vector<int>>
+{
+  vector<vector<int>> p = {{}};
+  for (auto e : s) {
+    for (auto b : decltype(p)(p)) {
+      b.push_back(e);
+      p.push_back(b);
+    }
+  }
+  return p;
+}
+
+inline auto generate_powerset_1(vector<int> s) -> vector<vector<int>>
+{
+  vector<vector<int>> p;
+  auto const n = 1 << s.size();
+  for (auto i = 0; i < n; i++) {
+    auto b = bitset<sizeof(decltype(i))>(i);
+    vector<int> q;
+    for (auto k = 0; k < b.size(); k++) {
+      if (b[k]) q.push_back(s[k]);
+    }
+    p.push_back(q);
+  }
+  return p;
+}
+
+vector<vector<int>> GeneratePowerSet(const vector<int>& s)
+{
+  return generate_powerset_0(s);
 }
 
 int main(int argc, char* argv[]) {
