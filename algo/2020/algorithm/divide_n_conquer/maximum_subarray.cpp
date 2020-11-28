72229aef02e58efc419ae6b3d536fc30f9be396a
Lance LI <heuristack@gmail.com>
2020-11-29 00:03:41 +0800

    programming: maximum subarray in dp ... [algo]

--- algo/2020/algorithm/divide_n_conquer/maximum_subarray.cpp
+++ algo/2020/algorithm/divide_n_conquer/maximum_subarray.cpp
@@ -9,10 +9,12 @@ using namespace std;
 
 #include "maximum_subarray_kadane.ipp"
 #include "maximum_subarray_dnc.ipp"
+#include "maximum_subarray_dp.ipp"
 
 int main()
 {
     vector<vector<int>> vv = {
+        {904,40,523,12,-335,-385,-124,481,-31},
         {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7},
         {5,-7,3,5,-2,4,-1},
         {-2,1,2},
@@ -22,6 +24,8 @@ int main()
         cout << get<2>(maximum_subarray(begin(v),end(v)));
         cout << "=";
         cout << maximum_subarray_kadane(v);
+        cout << "=";
+        cout << maximum_subarray_dp(v);
         cout << endl;
     }
 }
--- problems/2020/epi_judge_cpp/fibonacci.cc
+++ problems/2020/epi_judge_cpp/fibonacci.cc
@@ -1,12 +1,29 @@
 #include "test_framework/generic_test.h"
-int Fibonacci(int n) {
-  // TODO - you fill in here.
-  return -1;
+
+#include <unordered_map>
+using std::unordered_map;
+
+auto fibonacci(int n) -> int
+{
+    int a = 0, b = 1;
+    int f = n;
+    for (int i = 2; i <= n; i++) {
+        f = a + b; a = b; b = f;
+    }
+    return f;
+}
+
+int Fibonacci(int n)
+{
+  static unordered_map<int,int> m;
+  if (n == 0 || n == 1) return n;
+  else if (!m.count(n)) { m[n] = Fibonacci(n-1) + Fibonacci(n-2); }
+  return m[n];
 }
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"n"};
-  return GenericTestMain(args, "fibonacci.cc", "fibonacci.tsv", &Fibonacci,
+  return GenericTestMain(args, "fibonacci.cc", "fibonacci.tsv", &fibonacci,
                          DefaultComparator{}, param_names);
 }
