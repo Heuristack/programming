b166f516d4cc74b6ec3995bed6348133e29de68c
Lance LI <heuristack@gmail.com>
2020-12-18 00:18:28 +0800

    programming: game [algo]

--- algo/2020/searching/game/game.cpp
+++ algo/2020/searching/game/game.cpp
@@ -17,7 +17,7 @@ public:
     element(char v): value(v) {}
     bool operator < (element const & that) const { return this->value < that.value; }
 public:
-    char value = ' ';
+    char value = E;
 };
 
 auto operator << (ostream & s, element const & e) -> ostream &
@@ -31,22 +31,23 @@ public:
     using board_type = board<element>;
     using board_type::board_type;
 public:
-    auto get(int i, int j) const -> element
-    {
-        return this->operator[](i).operator[](j);
-    }
+    auto get(int i, int j) const -> element { return this->operator[](i).operator[](j); }
 
 public:
-    auto is_terminal() const -> bool;
+    struct terminal_utility { bool is_terminal; int utility; };
+    auto terminal_test() const -> terminal_utility;
 };
 
-auto state::is_terminal() const -> bool
+auto state::terminal_test() const -> state::terminal_utility
 {
     set<element> line;
+    int value = 0;
 
-    auto is_lined_up = [](decltype(line) const & line){
+    auto is_lined_up = [&line,&value](){
         if (line.size() == 1) {
-            if (auto e = line.begin()->value; (e == X || e == O)) {
+            if (auto e = line.begin()->value; e != E) {
+                if (e == X) value = +1;
+                if (e == O) value = -1;
                 return true;
             }
         }
@@ -54,66 +55,68 @@ auto state::is_terminal() const -> bool
     };
 
     // note : rows
-    for (int i = 0; i < size_m(i); i++) {
+    for (int i = 0; i < size_m(); i++) {
         line.clear();
-        for (int j = 0; j < size_n(i); j++) {
+        for (int j = 0; j < size_n(); j++) {
             line.insert(get(i,j));
         }
-        if (is_lined_up(line)) {
-        //  cout << "row: " << i << endl;
-            return true;
+        if (is_lined_up()) {
+//          cout << "row: " << i << endl;
+            return {true,value};
         }
     }
 
     // note : cols
-    for (int j = 0; j < size_n(j); j++) {
+    for (int j = 0; j < size_n(); j++) {
         line.clear();
-        for (int i = 0; i < size_m(j); i++) {
+        for (int i = 0; i < size_m(); i++) {
             line.insert(get(i,j));
         }
-        if (is_lined_up(line)) {
-        //  cout << "col: " << j << endl;
-            return true;
+        if (is_lined_up()) {
+//          cout << "col: " << j << endl;
+            return {true,value};
         }
     }
 
     // note : diag+
     line.clear();
-    for (int i = 0; i < size_m(i); i++) {
-        for (int j = 0; j < size_n(i); j++) {
+    for (int i = 0; i < size_m(); i++) {
+        for (int j = 0; j < size_n(); j++) {
             if (i == j) {
                 line.insert(get(i,j));
             }
         }
     }
-    if (is_lined_up(line)) {
-    //  cout << "diag+: " << endl;
-        return true;
+    if (is_lined_up()) {
+//      cout << "diag+: " << endl;
+        return {true,value};
     }
 
     // note : diag-
     line.clear();
-    for (int i = 0; i < size_m(i); i++) {
-        for (int j = 0; j < size_n(i); j++) {
+    for (int i = 0; i < size_m(); i++) {
+        for (int j = 0; j < size_n(); j++) {
             if (i + j == size_m(i) - 1) {
                 line.insert(get(i,j));
             }
         }
     }
-    if (is_lined_up(line)) {
-    //  cout << "diag-: " << endl;
-        return true;
+    if (is_lined_up()) {
+//      cout << "diag-: " << endl;
+        return {true,value};
     }
 
     // note : all filled
-    for (int i = 0; i < size_m(i); i++) {
-        for (int j = 0; j < size_n(i); j++) {
+    for (int i = 0; i < size_m(); i++) {
+        for (int j = 0; j < size_n(); j++) {
             if (get(i,j).value == E) {
-                return false;
+                return {false,value};
             }
         }
     }
-    return true;
+
+//  cout << "filled" << endl;
+    return {true,value};
 }
 
 auto operator << (ostream & s, state const & b) -> ostream &
@@ -130,11 +133,13 @@ int main()
 {
     state s
     {
+      {O,X,O},
       {X,X,O},
-      {X,X,O},
-      {O,O,E},
+      {O,O,X},
     };
     cout << s;
-    cout << s.is_terminal() << endl;
+    auto [is_terminal,utility] = s.terminal_test();
+    cout << is_terminal << "," << utility << endl;
+
 }
 
