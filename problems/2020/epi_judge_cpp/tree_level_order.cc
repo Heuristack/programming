8ebca562c25e9c22f978d0e2ec23b623585d7c70
Lance LI <heuristack@gmail.com>
2020-12-23 15:46:17 +0800

    programming: traverse binary tree by levels [prob]

--- problems/2020/epi_judge_cpp/tree_level_order.cc
+++ problems/2020/epi_judge_cpp/tree_level_order.cc
@@ -1,15 +1,35 @@
 #include <memory>
 #include <vector>
+#include <queue>
 
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 using std::vector;
+using std::queue;
 
-vector<vector<int>> BinaryTreeDepthOrder(
-    const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+vector<vector<int>> BinaryTreeDepthOrder(const unique_ptr<BinaryTreeNode<int>>& tree)
+{
+  vector<vector<int>> levels;
+  vector<int> nodes;
+  int depth = 0;
+  queue<BinaryTreeNode<int>*> q;
+  if (tree.get() != nullptr) q.push(tree.get());
+  int num = q.size();
+  while (!q.empty()) {
+    auto n = q.front();
+    nodes.push_back(n->data);
+    q.pop();
+    num--;
+    if (n->left)  q.push(n->left.get());
+    if (n->right) q.push(n->right.get());
+    if (!num) {
+      num = q.size();
+      depth++;
+      levels.emplace_back(std::move(nodes));
+    }
+  }
+  return levels;
 }
 
 int main(int argc, char* argv[]) {
