2b8a1a805a0044c45cef80b0b7ad020fe0314ae8
Lance LI <heuristack@gmail.com>
2020-12-23 23:42:27 +0800

    programming: adding flood fill [algo]

---
+++ algo/2020/graph/examples/flood_fill.cpp
@@ -0,0 +1,38 @@
+#include "implicit_graph.h"
+#include "algorithm/search.hpp"
+#include <iostream>
+#include "board.hpp"
+
+class element
+{
+public:
+    enum color { B = 0, W = 1 };
+public:
+    color c = B;
+};
+
+auto operator << (ostream & s, element const & e) -> ostream &
+{
+    return s << static_cast<int>(e.c);
+}
+
+class position
+{
+public:
+    position(int i, int j) : i(i), j(j) {}
+public:
+    int i = 0;
+    int j = 0;
+};
+
+auto operator << (ostream & s, position const & p) -> ostream &
+{
+    return s << "(" << p.i << "," << p.j << ")";
+}
+
+int main()
+{
+    board<element> b(10,10);
+    cout << b;
+}
+
