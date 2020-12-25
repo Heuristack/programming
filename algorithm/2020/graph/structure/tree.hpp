3ff14c3bfe23d1f8eb5d28cb614ab781c086c230
Lance LI <heuristack@gmail.com>
2020-12-25 11:55:24 +0800

    programming: add comment for tree [algo]

--- algorithm/2020/graph/structure/tree.hpp
+++ algorithm/2020/graph/structure/tree.hpp
@@ -14,3 +14,9 @@ public:
     virtual auto operator[](node_type const & n) -> set_type<edge_type> = 0;
 };
 
+// A problem-space graph is often used to represent a problem space.
+// Although most problem spaces correspond to graphs with more than one path between a pair of nodes,
+// for simplicity they are often represented as trees,
+// where the initial state is the root of the tree.
+// Korf, R.E., 1999. Artificial intelligence search algorithms.
+
