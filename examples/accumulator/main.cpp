548cdd55e82788fbf997d029e4b700d7eee6af00
Lance LI <heuristack@gmail.com>
2020-12-25 13:45:43 +0800

    programming: accumulators [boost]

--- examples/accumulator/main.cpp
+++ examples/accumulator/main.cpp
@@ -8,9 +8,11 @@ using namespace std;
 
 int main()
 {
-    accumulator_set<double, stats<tag::mean, tag::median, tag::moment<2>>> acc;
-    acc(1.2); acc(2.3); acc(3.4); acc(4.5); acc(5.6);
-    cout << "median = " << median(acc) << endl;
-    cout << "moment = " << moment<2>(acc) << endl;
+    accumulator_set<double, stats<tag::min, tag::max, tag::variance>> acc;
+    acc(3.1); acc(3.0); acc(3.2); acc(3.4); acc(3.3);
+    cout << "avg = " << mean(acc) << endl;
+    cout << "var = " << variance(acc) << endl;
+    cout << "min = " << extract::min(acc) << endl;
+    cout << "max = " << extract::max(acc) << endl;
 }
 
