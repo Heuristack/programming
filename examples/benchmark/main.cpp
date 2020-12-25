b6e8649b203ddbaf5190b3d799922cc754833ef7
Lance LI <heuristack@gmail.com>
2020-12-25 11:13:51 +0800

    programming: add benchmark example [benchmark]

--- examples/benchmark/main.cpp
+++ examples/benchmark/main.cpp
@@ -1,16 +1,9 @@
 #include <benchmark/benchmark.h>
 
-static void StringCreation(benchmark::State & state) {
-    for (auto _ : state) {
-        std::string created_string("hello");
-        benchmark::DoNotOptimize(created_string);
-    }
-}
-BENCHMARK(StringCreation);
-
 static void StringCreationMove(benchmark::State & state) {
-    std::string x = "hello";
     for (auto _ : state) {
+        std::string x = "hello";
+        benchmark::DoNotOptimize(x);
         std::string created_string_move(std::move(x));
         benchmark::DoNotOptimize(created_string_move);
     }
@@ -18,21 +11,13 @@ static void StringCreationMove(benchmark::State & state) {
 BENCHMARK(StringCreationMove);
 
 static void StringCreationCopy(benchmark::State & state) {
-    std::string x = "hello";
     for (auto _ : state) {
+        std::string x = "hello";
+        benchmark::DoNotOptimize(x);
         std::string created_string_copy(x);
         benchmark::DoNotOptimize(created_string_copy);
     }
 }
 BENCHMARK(StringCreationCopy);
-
-static void StringCreationCopyByChar(benchmark::State & state) {
-    char const * x = "hello";
-    for (auto _ : state) {
-        std::string created_string_by_char(x);
-        benchmark::DoNotOptimize(created_string_by_char);
-    }
-}
-BENCHMARK(StringCreationCopyByChar);
 
 BENCHMARK_MAIN();
