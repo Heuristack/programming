34aec398d6d35752f1f8ab5f5ba0027b3b4af891
Lance LI <heuristack@gmail.com>
2020-12-26 11:30:52 +0800

    programming: refactor [algo]

--- algorithm/2020/searching/state_space/state_expansion_recursion.cpp
+++ algorithm/2020/searching/state_space/state_expansion_recursion.cpp
@@ -49,7 +49,37 @@ public:
     auto explore(node e) -> void override;
 };
 
-#include "state_expansion_recursion.ipp"
+auto implicit_node_generation::explore(node e) -> void
+{
+    visit(e);
+    if (e.d < m) {
+        for (int b = 0; b < n; b++) {
+            node n(k++,b,e.d+1);
+            explore(n);
+        }
+    }
+}
+
+auto explicit_node_generation::generate(node e) -> vector<node>
+{
+    vector<node> v;
+    if (e.d < m) {
+        for (int b = 0; b < n; b++) {
+            node n(k++,b,e.d+1);
+            v.push_back(n);
+        }
+    }
+    return v;
+}
+
+auto explicit_node_generation::explore(node e) -> void
+{
+    visit(e);
+    for (node const & n : generate(e)) {
+        explore(n);
+    }
+}
+
 
 int main()
 {
