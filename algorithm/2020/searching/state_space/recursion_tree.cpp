6b3f3c5fdd57fa16a554d920f01a5d2d9ea57c7e
Lance LI <heuristack@gmail.com>
2020-12-26 11:35:28 +0800

    programming: refactor [algo]

--- algorithm/2020/searching/state_space/recursion_tree.cpp
+++ algorithm/2020/searching/state_space/recursion_tree.cpp
@@ -35,7 +35,16 @@ public:
     using recursion::recursion;
 
 public:
-    auto explore(node e) -> void override;
+    auto explore(node e) -> void override
+    {
+        visit(e);
+        if (e.d < m) {
+            for (int b = 0; b < n; b++) {
+                node n(k++,b,e.d+1);
+                explore(n);
+            }
+        }
+    }
 };
 
 class explicit_node_generation : public recursion
@@ -44,41 +53,27 @@ public:
     using recursion::recursion;
 
 public:
-    auto generate(node e) -> vector<node>;
-public:
-    auto explore(node e) -> void override;
-};
+    auto generate(node e) -> vector<node>
+    {
+        vector<node> v;
+        if (e.d < m) {
+            for (int b = 0; b < n; b++) {
+                node n(k++,b,e.d+1);
+                v.push_back(n);
+            }
+        }
+        return v;
+    }
 
-auto implicit_node_generation::explore(node e) -> void
-{
-    visit(e);
-    if (e.d < m) {
-        for (int b = 0; b < n; b++) {
-            node n(k++,b,e.d+1);
+public:
+    auto explore(node e) -> void override
+    {
+        visit(e);
+        for (node const & n : generate(e)) {
             explore(n);
         }
     }
-}
-
-auto explicit_node_generation::generate(node e) -> vector<node>
-{
-    vector<node> v;
-    if (e.d < m) {
-        for (int b = 0; b < n; b++) {
-            node n(k++,b,e.d+1);
-            v.push_back(n);
-        }
-    }
-    return v;
-}
-
-auto explicit_node_generation::explore(node e) -> void
-{
-    visit(e);
-    for (node const & n : generate(e)) {
-        explore(n);
-    }
-}
+};
 
 
 int main()
