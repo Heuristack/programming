6eada0e54a1dbbcb13561e1eaa08ee37e7b42702
Lance LI <heuristack@gmail.com>
2020-12-26 16:42:12 +0800

    programming: refactor [algo]

--- algorithm/2020/algorithm/divide_n_conquer/divide_n_conquer.cpp
+++ algorithm/2020/algorithm/divide_n_conquer/divide_n_conquer.cpp
@@ -2,15 +2,20 @@
 #include <iomanip>
 using namespace std;
 
+auto conquer(int k, int p, int m, int r, int d) -> void
+{
+    cout << setw(2) << setfill('0') << k++ << ":";
+    cout << "(" << p << "," << r << ")";
+    cout << "(" << m << "-" << d << ")";
+    cout << endl;
+}
+
 auto divide(int p, int r) -> void
 {
     static int k = 0;
     int d = r - p;
     int m = p + d/2;
-    cout << setw(2) << setfill('0') << k++ << ":";
-    cout << "(" << p << "," << r << ")";
-    cout << "(" << m << "-" << d << ")";
-    cout << endl;
+    conquer(k,p,m,r,d);
     if (d <= 0) return;
     divide(p,m);
     divide(m+1,r);
