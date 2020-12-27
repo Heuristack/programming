0ae35e3aac58e4b768c460c45adafadaffa61191
Lance LI <heuristack@gmail.com>
2020-12-27 09:50:15 +0800

    programming: refactor [algo]

--- examples/benchmark/main.cpp
+++ examples/benchmark/main.cpp
@@ -2,7 +2,7 @@
 
 static void StringCreationMove(benchmark::State & state) {
     for (auto _ : state) {
-        std::string x = "hello";
+        std::string x = "move is faster than copy when string is longer";
         benchmark::DoNotOptimize(x);
         std::string created_string_move(std::move(x));
         benchmark::DoNotOptimize(created_string_move);
@@ -12,7 +12,7 @@ BENCHMARK(StringCreationMove);
 
 static void StringCreationCopy(benchmark::State & state) {
     for (auto _ : state) {
-        std::string x = "hello";
+        std::string x = "move is faster than copy when string is longer";
         benchmark::DoNotOptimize(x);
         std::string created_string_copy(x);
         benchmark::DoNotOptimize(created_string_copy);
