b75fc8f744d76b1074de8ad97fade91ccd608f31
Lance LI <heuristack@gmail.com>
2021-01-05 16:12:03 +0800

    programming: BST in-order DFS and BFS [algo]

--- algorithm/2020/searching/tree_search/binary_search_tree.cpp
+++ algorithm/2020/searching/tree_search/binary_search_tree.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
 #include <limits>
+#include <iostream>
 
 using namespace std;
 
@@ -34,7 +37,32 @@ int main()
                     nullptr,
                     new node_type(53))));
 
-    binary_search_tree_traverse<node_type>(tree); cout << endl;
+    // note : BFS top-down explore
+    vector<node_type> levels;
+    binary_search_tree_explore<node_type>(tree, [&levels](node_type * n){
+        levels.push_back(*n);
+    });
+    for (auto const & n : levels) {
+        cout << n;
+    }
+    cout << endl;
+
+    // note : DFS in-order traverse
+    vector<node_type> in_order;
+    binary_search_tree_traverse<node_type>(tree, [&in_order](node_type * n){
+        in_order.push_back(*n);
+    });
+    for (auto const & n : in_order) {
+        cout << n;
+    }
+    cout << endl;
+
+    // note : DFS in-order path is sorted -> BST
+    cout << is_sorted(begin(in_order),end(in_order),[](auto const & a, auto const & b){
+        return a.value < b.value;
+    }) << endl;
+
+    // note : DFS to verify BST property
     cout << is_binary_search_tree<node_type>(tree) << endl;
     if (auto n = binary_search_tree_search<node_type>(tree,53); n) {
         cout << n->value << endl;
--- algorithm/2020/searching/tree_search/traverse.ipp
+++ algorithm/2020/searching/tree_search/traverse.ipp
@@ -1,9 +1,24 @@
-template <typename node_t>
-auto binary_search_tree_traverse(typename node_t::link_type tree)
+template <typename node_t, typename vistor_t>
+auto binary_search_tree_traverse(typename node_t::link_type tree, vistor_t f)
 {
     if (!tree) return;
-    binary_search_tree_traverse<node_t>(tree->ltree);
-    cout << (*tree);
-    binary_search_tree_traverse<node_t>(tree->rtree);
+    binary_search_tree_traverse<node_t>(tree->ltree,f);
+    f(tree);
+    binary_search_tree_traverse<node_t>(tree->rtree,f);
+}
+
+template <typename node_t, typename vistor_t>
+auto binary_search_tree_explore(typename node_t::link_type tree, vistor_t f)
+{
+    queue<typename node_t::link_type> q;
+    q.push(tree);
+    while (!q.empty()) {
+        auto p = q.front();
+        q.pop();
+        if (!p) continue;
+        f(p);
+        q.push(p->ltree);
+        q.push(p->rtree);
+    }
 }
 
