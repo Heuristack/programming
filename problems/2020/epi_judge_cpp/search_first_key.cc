bff7c6840fbcf900afcd46d39e492f6ab70b146f
Lance LI <heuristack@gmail.com>
2021-01-05 17:28:58 +0800

    programming: first k search [prob]

--- problems/2020/epi_judge_cpp/search_first_key.cc
+++ problems/2020/epi_judge_cpp/search_first_key.cc
@@ -1,11 +1,35 @@
+#include <algorithm>
 #include <vector>
+#include <cassert>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::lower_bound;
+using std::is_sorted;
 
-int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+int SearchFirstOfK0(const vector<int>& A, int k)
+{
+  assert(is_sorted(begin(A),end(A)));
+  auto e = lower_bound(begin(A),end(A),k);
+  if (e != end(A) && *e == k ) return e - begin(A);
+  return -1;
+}
+
+int SearchFirstOfK(vector<int> const & A, int k)
+{
+  if (A.empty()) return -1;
+  int p = 0, q = size(A) - 1;
+  int d = q - p;
+  int m = p + d/2;
+  int e = -1;
+  while (d >= 0) {
+    if (k < A[m]) { q = m - 1; }
+    else if (k == A[m]) { e = m; q = m - 1; }
+    else { p = m + 1; }
+    d = q - p;
+    m = p + d/2;
+  }
+  return e;
 }
 
 int main(int argc, char* argv[]) {
@@ -14,3 +38,4 @@ int main(int argc, char* argv[]) {
   return GenericTestMain(args, "search_first_key.cc", "search_first_key.tsv",
                          &SearchFirstOfK, DefaultComparator{}, param_names);
 }
+
