d64b683213dcf7b182a24659af904e4f7c922206
Lance LI <heuristack@gmail.com>
2021-01-06 23:04:20 +0800

    programming: merge sorted arrays [algo]

--- problems/2020/epi_judge_cpp/sorted_arrays_merge.cc
+++ problems/2020/epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,10 +1,41 @@
 #include <vector>
+#include <queue>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
-vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+using std::queue;
+using std::priority_queue;
+
+vector<int> MergeSortedArrays(vector<vector<int>> const & sorted_arrays)
+{
+    vector<int> m;
+
+    struct index
+    {
+        using iterator = vector<int>::const_iterator;
+        index(iterator c, iterator e) : current(c), end(e) {}
+        iterator current;
+        iterator end;
+        bool operator < (index const & that) const { return *current > *that.current; }
+    };
+
+    priority_queue<index> q;
+    for (auto & v : sorted_arrays) {
+        if (!v.empty()) {
+            q.emplace(begin(v),end(v));
+        }
+    }
+    while (!q.empty()) {
+        auto i = q.top();
+        q.pop();
+        if (i.current != i.end) {
+            m.emplace_back(*i.current);
+            q.emplace(next(i.current),i.end);
+        }
+    }
+
+    return m;
 }
 
 int main(int argc, char* argv[]) {
@@ -14,3 +45,4 @@ int main(int argc, char* argv[]) {
                          "sorted_arrays_merge.tsv", &MergeSortedArrays,
                          DefaultComparator{}, param_names);
 }
+
