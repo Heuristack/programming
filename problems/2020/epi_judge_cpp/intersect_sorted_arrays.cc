28a1b48c2fae0c3fab57d2b53e76bef58eb2142a
Lance LI <heuristack@gmail.com>
2021-01-06 23:26:01 +0800

    programming: intersect of two arrays [prob]

--- problems/2020/epi_judge_cpp/intersect_sorted_arrays.cc
+++ problems/2020/epi_judge_cpp/intersect_sorted_arrays.cc
@@ -3,10 +3,21 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-vector<int> IntersectTwoSortedArrays(const vector<int>& A,
-                                     const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+vector<int> IntersectTwoSortedArrays(vector<int> const & A, vector<int> const & B)
+{
+  vector<int> v;
+  auto pa = begin(A), ea = end(A);
+  auto pb = begin(B), eb = end(B);
+  while ((pa != ea) && (pb != eb)) {
+    if (*pa == *pb) {
+      if (v.empty() || v.back() != *pa)
+        v.push_back(*pa);
+      pa++; pb++;
+    }
+    else if (*pa < *pb) pa++;
+    else pb++;
+  }
+  return v;
 }
 
 int main(int argc, char* argv[]) {
@@ -16,3 +27,4 @@ int main(int argc, char* argv[]) {
       args, "intersect_sorted_arrays.cc", "intersect_sorted_arrays.tsv",
       &IntersectTwoSortedArrays, DefaultComparator{}, param_names);
 }
+
