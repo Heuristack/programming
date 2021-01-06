67f083886714ef3e51913512ebe8ef63a6179b1a
Lance LI <heuristack@gmail.com>
2021-01-06 23:41:50 +0800

    programming: merge two sorted arrays [algo]

--- problems/2020/epi_judge_cpp/two_sorted_arrays_merge.cc
+++ problems/2020/epi_judge_cpp/two_sorted_arrays_merge.cc
@@ -1,14 +1,27 @@
+#include <cassert>
 #include <vector>
+#include <utility>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::move;
 
-void MergeTwoSortedArrays(vector<int>& A, int m, const vector<int>& B, int n) {
-  // TODO - you fill in here.
-  return;
+void MergeTwoSortedArrays(vector<int> & A, int m, const vector<int> & B, int n)
+{
+  assert(A.size() >= m && B.size() >= n);
+  vector<int> v;
+  int i = 0, j = 0;
+  while (i != m && j != n) {
+    if (A[i] <= B[j]) { v.push_back(A[i++]); }
+    else { v.push_back(B[j++]); }
+  }
+  while (i != m) v.push_back(A[i++]);
+  while (j != n) v.push_back(B[j++]);
+  A = move(v);
 }
-vector<int> MergeTwoSortedArraysWrapper(vector<int> A, int m,
-                                        const vector<int>& B, int n) {
+
+vector<int> MergeTwoSortedArraysWrapper(vector<int> A, int m, const vector<int>& B, int n)
+{
   MergeTwoSortedArrays(A, m, B, n);
   return A;
 }
