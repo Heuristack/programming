67ddd1dd93fe9a780488877164c5ef117cfa80de
Lance LI <heuristack@gmail.com>
2021-01-08 11:05:32 +0800

    programming: heap usages [algo]

--- algorithm/2020/structure/heap/priority_queue_usages.cpp
+++ algorithm/2020/structure/heap/priority_queue_usages.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 struct element
 {
+public:
     explicit element(double m) : m(m), s(to_string(m)) {}
 
-    struct less
+public:
+    struct greater
     {
         bool operator () (element const & a, element const & b)
         {
@@ -16,11 +18,12 @@ struct element
         }
     };
 
-    bool operator< (element const & that) const
+    bool operator < (element const & that) const
     {
         return m < that.m;
     }
 
+public:
     double m;
     string s;
 };
@@ -32,14 +35,15 @@ auto test(priority_queue_t & pq)
     pq.emplace(1.0);
     pq.emplace(4.0);
     while (!pq.empty()) {
-        cout << pq.top().m << endl;
+        cout << "(" << pq.top().m << ")";
         pq.pop();
     }
+    cout << endl;
 }
 
 int main()
 {
-    priority_queue<element, vector<element>, element::less> pq_s;
+    priority_queue<element, vector<element>, element::greater> pq_s;
     priority_queue<element> pq_m;
     test(pq_s);
     test(pq_m);
