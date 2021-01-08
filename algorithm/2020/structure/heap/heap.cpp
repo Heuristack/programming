927636b5a0ad847def96c56c48e9d2b96835cd14
Lance LI <heuristack@gmail.com>
2021-01-08 10:52:10 +0800

    programming: heap usages [algo]

--- algorithm/2020/structure/heap/heap.cpp
+++ algorithm/2020/structure/heap/heap.cpp
@@ -19,17 +19,29 @@ auto to_string(iterator b, iterator e) -> string
 
 int main()
 {
+    // note : make a heap
     vector<int> heap = {3,1,4,1,5,9,2,6};
     make_heap(begin(heap),end(heap));
     cout << to_string(begin(heap),end(heap)) << endl;
 
+    // note : append 5,3 to heap
     heap.push_back(5);
     heap.push_back(3);
     cout << to_string(begin(heap),end(heap)) << endl;
 
+    // note : the appended 5,3 shouldn't be in heap
     auto heap_end = is_heap_until(begin(heap),end(heap));
     cout << to_string(begin(heap),heap_end) << endl;
 
+    // note : push 5 into the heap
     push_heap(begin(heap),next(heap_end));
+    cout << to_string(begin(heap),end(heap)) << endl;
+
+    // note : heap is increased by 2, since 3 is accentally already in heap
+    heap_end = is_heap_until(begin(heap),end(heap));
+    cout << to_string(begin(heap),heap_end) << endl;
+
+    // note : pop the maximum element out from the heap
+    pop_heap(begin(heap),end(heap));
     cout << to_string(begin(heap),end(heap)) << endl;
 }
