38e6418869b58109dd5903d2a6da5b43d63ec8d7
Lance LI <heuristack@gmail.com>
2021-01-18 22:06:26 +0800

    programming: increment an integer in arbitrary-precision [prob]

--- problems/2020/epi_judge_cpp/int_as_array_increment.cc
+++ problems/2020/epi_judge_cpp/int_as_array_increment.cc
@@ -2,9 +2,19 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
-vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+
+vector<int> PlusOne(vector<int> A)
+{
+  A.back()++;
+  int b = 10;
+  int q = 0, r = 0;
+  for (auto i = rbegin(A); i != rend(A); i++) {
+    q = (*i)/b;
+    r = (*i)%b;
+    if (i != rend(A) && q > 0) { (*next(i)) += q; (*i) = r; }
+  }
+  if (q > 0) A.insert(A.begin(),q);
+  return A;
 }
 
 int main(int argc, char* argv[]) {
@@ -14,3 +24,4 @@ int main(int argc, char* argv[]) {
                          "int_as_array_increment.tsv", &PlusOne,
                          DefaultComparator{}, param_names);
 }
+
