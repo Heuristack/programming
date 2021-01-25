f504810ef7595b2c63005a49aac8c110e838a8f1
Lance LI <heuristack@gmail.com>
2021-01-25 21:41:49 +0800

    programming: sieve [prob]

--- problems/2020/epi_judge_cpp/prime_sieve.cc
+++ problems/2020/epi_judge_cpp/prime_sieve.cc
@@ -1,11 +1,28 @@
 #include <vector>
+#include <deque>
+#include <iostream>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::deque;
+using std::cout;
+using std::endl;
+
 // Given n, return all primes up to and including n.
-vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+vector<int> GeneratePrimes(int n)
+{
+  vector<int> primes;
+  deque<bool> is_prime(n+1,true);
+  is_prime[0] = is_prime[1] = false;
+  for (int p = 2; p <= n; p++) {
+    if (is_prime[p]) {
+      primes.emplace_back(p);
+      for (int i = p; i <= n; i += p) {
+        is_prime[i] = false;
+      }
+    }
+  }
+  return primes;
 }
 
 int main(int argc, char* argv[]) {
