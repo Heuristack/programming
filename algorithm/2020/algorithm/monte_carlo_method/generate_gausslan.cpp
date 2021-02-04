caa223cd1010b15decfeee45f9837bc5ea07b191
Lance LI <heuristack@gmail.com>
2021-02-04 23:58:33 +0800

    programming: generate normal distribution into histogram [algo]

--- algorithm/2020/algorithm/monte_carlo_method/generate_gausslan.cpp
+++ algorithm/2020/algorithm/monte_carlo_method/generate_gausslan.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include <boost/histogram.hpp>
+#include <boost/format.hpp>
+#include <functional>
+#include <algorithm>
 #include <random>
 #include <vector>
+#include <sstream>
+#include <iostream>
 
+using namespace boost::histogram;
 using namespace std;
 
 template <typename device = random_device, typename engine = default_random_engine,
@@ -18,11 +24,23 @@ auto generate(result_type mean = 0, result_type stddev = 1) -> result_type
 int main()
 {
     vector<double> values;
+    for (int i = 0; i < 1000; i++) { values.push_back(generate(0.0,0.4)); }
+
     double sigma = 0;
     double mean = 0;
-    for (int i = 0; i < 1000; i++) { values.push_back(generate(5.0,4.0)); }
     for (double value : values) { sigma += value; }
     mean = sigma/values.size();
     cout << mean << endl;
+
+    auto histogram = make_histogram(axis::regular<>(9, -1.0, 1.0, "x"));
+    std::for_each(values.begin(), values.end(), std::ref(histogram));
+
+    std::ostringstream os;
+    for (auto && x : indexed(histogram, coverage::all)) {
+        os << boost::format("bin %2i [%4.1f, %4.1f): %i\n")
+        % x.index() % x.bin().lower() % x.bin().upper() % *x;
+    }
+
+    std::cout << os.str() << std::flush;
 }
 
