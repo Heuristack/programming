fb370c691c56bbd85b84d9ebde9d2a36304d8ce3
Lance LI <heuristack@gmail.com>
2021-02-06 21:59:38 +0800

    programming: implement interval merge [algo]

--- algorithm/2020/structure/interval/merge.cpp
+++ algorithm/2020/structure/interval/merge.cpp
@@ -1,18 +1,35 @@
 #include <algorithm>
 #include <vector>
+#include <map>
 #include <iostream>
 #include "interval.h"
 
 using namespace std;
 
-int main()
+bool is_open(vector<endpoint> const & endpoints)
 {
-    vector<interval> intervals = {{-4,-1},{0,2},{3,6},{7,9},{11,12},{14,17},{1,8}};
-    for (auto i : intervals) cout << i; cout << endl;
+    map<int,int> intervals_status;
+    for (auto const & e : endpoints) {
+        intervals_status[e.i] = e.m;
+    }
+    return any_of(begin(intervals_status),end(intervals_status),[](auto const & is){
+        if (auto const & [i,s] = is; s == 0) { return true; }
+        else return false;
+    });
+}
+
+auto merge(vector<endpoint> & endpoints) -> interval
+{
+    interval i(endpoints.front().e,endpoints.back().e);
+    endpoints.clear();
+    return i;
+}
+
+auto merge(vector<interval> & intervals) -> vector<interval>
+{
     sort(begin(intervals),end(intervals),[](auto const & a, auto const & b) {
         return a.p < b.p;
     });
-    for (auto i : intervals) cout << i; cout << endl;
 
     vector<endpoint> endpoints;
     for (int i = 0; i < intervals.size(); i++) {
@@ -22,22 +39,31 @@ int main()
     sort(begin(endpoints),end(endpoints),[](auto const & a, auto const & b) {
         return a.e < b.e;
     });
-    for (auto e : endpoints) cout << e; cout << endl;
 
     vector<interval> results;
     vector<endpoint> stack;
     for (auto e : endpoints) {
-        if (e.m == 0) {
-            stack.push_back(e);
-        }
-        else {
-            if (stack.back().i == e.i) {
+        if (e.m == 1) {
+            if (auto b = stack.back(); b.i == e.i) {
                 stack.pop_back();
-                results.push_back(intervals[e.i]);
+                if (stack.empty()) {
+                    results.emplace_back(b.e,e.e);
+                }
+                continue;
             }
         }
+        stack.push_back(e);
+        if (!is_open(stack)) {
+            results.push_back(merge(stack));
+        }
     }
-    for (auto e : stack) cout << e; cout << endl;
-    for (auto i : results) cout << i; cout << endl;
+    return results;
+}
+
+
+int main()
+{
+    vector<interval> intervals = {{-4,-1},{0,2},{3,6},{7,9},{11,12},{14,17},{1,8}};
+    for (auto i : merge(intervals)) cout << i; cout << endl;
 }
 
