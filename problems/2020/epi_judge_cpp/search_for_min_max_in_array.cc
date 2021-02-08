2dd0e502cd1a3bed32f10fe287c672afed3294f4
Lance LI <heuristack@gmail.com>
2021-02-08 11:13:37 +0800

    programming: simultaneous minmax [prob]

--- problems/2020/epi_judge_cpp/search_for_min_max_in_array.cc
+++ problems/2020/epi_judge_cpp/search_for_min_max_in_array.cc
@@ -1,16 +1,28 @@
+#include <algorithm>
 #include <vector>
+#include <limits>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
-using std::vector;
-struct MinMax {
-  int smallest, largest;
-};
 
-MinMax FindMinMax(const vector<int>& A) {
-  // TODO - you fill in here.
-  return {0, 0};
+using std::vector;
+using std::min;
+using std::max;
+using std::numeric_limits;
+
+struct MinMax { int smallest, largest; };
+
+MinMax FindMinMax(vector<int> const & a)
+{
+  int min_num = numeric_limits<int>::max();
+  int max_num = numeric_limits<int>::min();
+  for (auto num : a) {
+    min_num = min(min_num,num);
+    max_num = max(max_num,num);
+  }
+  return {min_num,max_num};
 }
+
 namespace test_framework {
 template <>
 struct SerializationTrait<MinMax> : UserSerTrait<MinMax, int, int> {};
@@ -32,3 +44,4 @@ int main(int argc, char* argv[]) {
                          "search_for_min_max_in_array.tsv", &FindMinMax,
                          DefaultComparator{}, param_names);
 }
+
