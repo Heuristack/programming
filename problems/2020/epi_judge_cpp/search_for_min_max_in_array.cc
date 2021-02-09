2154c93f82649ede3c453c1db034f59aef945d95
Lance LI <heuristack@gmail.com>
2021-02-09 16:40:42 +0800

    programming: minmax optimization [prob]

--- problems/2020/epi_judge_cpp/search_for_min_max_in_array.cc
+++ problems/2020/epi_judge_cpp/search_for_min_max_in_array.cc
@@ -8,17 +8,36 @@
 using std::vector;
 using std::min;
 using std::max;
+using std::minmax;
 using std::numeric_limits;
 
 struct MinMax { int smallest, largest; };
 
-MinMax FindMinMax(vector<int> const & a)
+MinMax FindMinMax0(vector<int> const & a)
 {
   int min_num = numeric_limits<int>::max();
   int max_num = numeric_limits<int>::min();
   for (auto num : a) {
     min_num = min(min_num,num);
     max_num = max(max_num,num);
+  }
+  return {min_num,max_num};
+}
+
+MinMax FindMinMax(vector<int> const & a)
+{
+  auto n = a.size();
+  if (n == 1) return {a[0],a[0]};
+  int min_num = numeric_limits<int>::max();
+  int max_num = numeric_limits<int>::min();
+  for (int i = 0; i + 1 < n; i += 2) {
+    auto [p,q] = minmax(a[i],a[i+1]);
+    min_num = min(min_num,p);
+    max_num = max(max_num,q);
+  }
+  if (n % 2) {
+    min_num = min(min_num,a[n-1]);
+    max_num = max(max_num,a[n-1]);
   }
   return {min_num,max_num};
 }
