fdf175795041dfd6b396febe8eef131b3f9883ee
Lance LI <heuristack@gmail.com>
2021-02-10 12:46:08 +0800

    programming: refactor bst [algo]

--- algorithm/2020/searching/tree_search/binary_search_tree.cpp
+++ algorithm/2020/searching/tree_search/binary_search_tree.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 #include "binary_tree/binary_tree.hpp"
+#include "binary_tree/data.ipp"
 #include "traverse.ipp"
 #include "property.ipp"
 #include "search.ipp"
@@ -14,28 +15,7 @@ using namespace std;
 int main()
 {
     using node_type = node<int>;
-    typename node_type::link_type tree =
-        new node_type(19,
-            new node_type(7,
-                new node_type(3,
-                    new node_type(2),
-                    new node_type(5)),
-                new node_type(11,
-                    nullptr,
-                    new node_type(17,
-                        new node_type(13),
-                        new node_type(20)))), // breaks BST property
-            new node_type(43,
-                new node_type(23,
-                    nullptr,
-                    new node_type(37,
-                        new node_type(29,
-                            nullptr,
-                            new node_type(31)),
-                        new node_type(41))),
-                new node_type(47,
-                    nullptr,
-                    new node_type(53))));
+    node_type * tree = binary_tree_3();
 
     // note : BFS top-down explore
     vector<node_type> levels;
