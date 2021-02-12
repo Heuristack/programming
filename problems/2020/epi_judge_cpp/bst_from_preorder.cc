053f8dbe14ef240a02cdb71d713d1b0e4084262b
Lance LI <heuristack@gmail.com>
2021-02-12 12:44:57 +0800

    programming: construct bst using pre-order sequence [prob]

--- problems/2020/epi_judge_cpp/bst_from_preorder.cc
+++ problems/2020/epi_judge_cpp/bst_from_preorder.cc
@@ -1,14 +1,28 @@
+#include <algorithm>
 #include <memory>
 
 #include "bst_node.h"
 #include "test_framework/generic_test.h"
+
+using std::make_unique;
 using std::unique_ptr;
 using std::vector;
+using std::find_if;
 
-unique_ptr<BstNode<int>> RebuildBSTFromPreorder(
-    const vector<int>& preorder_sequence) {
-  // TODO - you fill in here.
-  return nullptr;
+unique_ptr<BstNode<int>> RebuildBSTFromPreorder(vector<int> const & preorder_sequence)
+{
+  unique_ptr<BstNode<int>> n;
+  if (preorder_sequence.empty()) return n;
+  n = make_unique<BstNode<int>>(*begin(preorder_sequence));
+  auto first_greater = find_if(begin(preorder_sequence),end(preorder_sequence),
+      [&preorder_sequence](auto i){ return i > *begin(preorder_sequence); });
+  vector<int> ltree_sequence(next(begin(preorder_sequence)),first_greater);
+  vector<int> rtree_sequence(first_greater,end(preorder_sequence));
+  auto ltree = RebuildBSTFromPreorder(ltree_sequence);
+  auto rtree = RebuildBSTFromPreorder(rtree_sequence);
+  n->left = move(ltree);
+  n->right = move(rtree);
+  return n;
 }
 
 int main(int argc, char* argv[]) {
@@ -18,3 +32,4 @@ int main(int argc, char* argv[]) {
                          &RebuildBSTFromPreorder, DefaultComparator{},
                          param_names);
 }
+
