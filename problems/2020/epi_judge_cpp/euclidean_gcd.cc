284f9e3ae128f2de09422749d3321d35dc875b66
Lance LI <heuristack@gmail.com>
2021-02-14 22:22:19 +0800

    programming: greatest common divisor [prob]

--- problems/2020/epi_judge_cpp/euclidean_gcd.cc
+++ problems/2020/epi_judge_cpp/euclidean_gcd.cc
@@ -1,12 +1,14 @@
 #include "test_framework/generic_test.h"
-long long Gcd(long long x, long long y) {
-  // TODO - you fill in here.
-  return 0;
+
+long long gcd(long long a, long long b)
+{
+  if (b) return gcd(b,a%b);
+  else return a;
 }
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
-  return GenericTestMain(args, "euclidean_gcd.cc", "gcd.tsv", &Gcd,
+  return GenericTestMain(args, "euclidean_gcd.cc", "gcd.tsv", &gcd,
                          DefaultComparator{}, param_names);
 }
