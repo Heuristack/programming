7310a67831eda92d5b493151b814739544d40a04
Lance LI <heuristack@gmail.com>
2021-03-10 22:25:39 +0800

    programming: add bootcamp examples linked list [algo]

--- algorithm/2021/01_linked_list.cpp
+++ algorithm/2021/01_linked_list.cpp
@@ -1,8 +1,9 @@
 /**
  *
- * Bootcamp01: Implement linked_list structure and traverse
+ * Bootcamp01: Implement a linked_list structure
  *
 **/
+
 #include <iostream>
 #include <vector>
 
@@ -18,12 +19,18 @@ public:
 
 public:
     node(data_type d, link_type n) : data(d),next(n) {}
-    explicit node(data_type d) : node(d, nullptr) {}
+    explicit node(data_type d) : node(d,nullptr) {}
 
 public:
     data_type data;
     link_type next;
 };
+
+template <typename data_t>
+auto link(node<data_t> & a, node<data_t> & b)
+{
+    a.next = &b;
+}
 
 template <typename data_t>
 ostream & operator << (ostream & s, node<data_t> const & n)
@@ -33,8 +40,16 @@ ostream & operator << (ostream & s, node<data_t> const & n)
 
 int main()
 {
-    using node_type = node<int>;
-    node_type n(10,nullptr);
-    cout << n << endl;
+    node<int> n0(0);
+    node<int> n1(1); link(n0,n1);
+    node<int> n2(2); link(n1,n2);
+    node<int> n3(3); link(n2,n3);
+    node<int> n4(4); link(n3,n4);
+
+    node<int>::link_type list = &n0;
+
+    for (node<int> * p = list; p != nullptr; p = p->next) {
+        cout << (*p) << endl;
+    }
 }
 
