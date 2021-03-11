71ba416c470f037c0e96fac812b5e2be4106ec71
Lance LI <heuristack@gmail.com>
2021-03-11 23:44:15 +0800

    programming: add bootcamp examples binary tree [algo]

---
+++ algorithm/2021/02_binary_tree.cpp
@@ -0,0 +1,37 @@
+/**
+ *
+ * Bootcamp02: Implement a binary tree structure
+ *
+**/
+#include <iostream>
+using namespace std;
+
+template <typename data_t>
+class node
+{
+public:
+    using data_type = data_t;
+    using node_type = node<data_type>;
+    using link_type = node_type*;
+public:
+    node(data_type v, link_type l, link_type r) : value(v),ltree(l),rtree(r) {}
+    explicit node(data_type v) : node(v,nullptr,nullptr) {}
+
+public:
+    data_type value;
+    link_type ltree;
+    link_type rtree;
+};
+
+template <typename data_t>
+ostream & operator << (ostream & s, node<data_t> const & n)
+{
+    return s << "(" << n.value << ")";
+}
+
+int main()
+{
+    node<int> root(0);
+    cout << root << endl;
+}
+
